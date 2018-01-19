5b2a6c78cb2683aef6086e3a207267b29dd22b04
dongyf <1471404820@qq.com>
2018-01-19 16:03:56 +0800

    添加图片上传功能

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var ueditor = require('ueditor');
 var app = express();
 
 // view engine setup
@@ -21,6 +21,34 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//ueditor
+app.use("/libs/ueditor/getImg", ueditor(path.join(__dirname, 'public'), function (req, res, next) {
+
+    // ueditor 客户发起上传图片请求
+    if (req.query.action === 'uploadimage') {
+        var foo = req.ueditor;
+
+        var imgname = req.ueditor.filename;
+
+        var img_url = '/upload';
+        res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
+    }
+
+    //  客户端发起图片列表请求
+    else if (req.query.action === 'listimage') {
+        var dir_url = '/upload';
+        res.ue_list(dir_url);  // 客户端会列出 dir_url 目录下的所有图片
+    }
+
+    // 客户端发起其它请求
+    else {
+
+        res.setHeader('Content-Type', 'application/json');
+        res.redirect('/libs/ueditor/nodejs/config.json')
+    }
+
+}));
 
 // sesssion应用的配置
 app.use(session({
